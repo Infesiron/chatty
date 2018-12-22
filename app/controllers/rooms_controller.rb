e1d037fa10daec3685186d60baf0cdd807932c6b
Roman <doublebabax@gmail.com>
2018-12-22 14:57:12 +0300

    CRUD rooms by admin

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_user!, :ensure_admin, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, :ensure_admin, only: [:new, :edit, :update, :destroy]
   before_action :set_room, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -7,11 +7,17 @@ class RoomsController < ApplicationController
   end
 
   def new
-      
+    @room = Room.new
   end
 
   def create
-
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:notice] = "Room successfully created!"
+      redirect_to rooms_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -23,15 +29,25 @@ class RoomsController < ApplicationController
   end
 
   def update
-
+    if @room.update(room_params)
+      flash[:notice] = "Room successfully updated!"
+      redirect_to rooms_path
+    else
+      render :edit
+    end
   end
 
   def destroy
     @room.destroy
+    flash[:notice] = "#{@room.title} room successfully deleted!"
     redirect_to rooms_path
   end
 
   private
+
+  def room_params
+    params.require(:room).permit(:title, :hidden)
+  end
 
   def set_room
     @room = Room.find(params[:id])
@@ -39,7 +55,7 @@ class RoomsController < ApplicationController
 
   def ensure_admin
     unless current_user.admin?
-      flash[:alert] = "This action available ony for admins"
+      flash[:alert] = "This action available ony for admins!"
       redirect_to root_path
     end
   end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   validates :title, presence: true
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
